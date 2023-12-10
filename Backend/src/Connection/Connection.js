93f44838cc0e29237c46c08ae98e1f04aabdd216
Avanish_sakhareliya <avanishsakhareliya@gmail.com>
2023-12-10 17:26:13 +0530

    add create table

--- Backend/src/Connection/Connection.js
+++ Backend/src/Connection/Connection.js
@@ -15,6 +15,29 @@ db.connect((err) => {
     return;
   }
   console.log('Connected to MySQL!');
+
+  const createUserIndexTable = `
+  CREATE TABLE IF NOT EXISTS (
+    srno INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    token VARCHAR(500),
+    role ENUM('admin', 'customer') NOT NULL
+  )
+`;
+
+  // Execute the query to create the table
+  db.query(createUserIndexTable, (err, result) => {
+    if (err) {
+      console.error('Error creating userindex table:', err);
+    } else {
+      console.log('userindex table created successfully');
+    }
+    db.end();
+  });
+
 });
 
 module.exports = db; // Export the db connection
